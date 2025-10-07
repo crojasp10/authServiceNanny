667381c27d8f14ec44a525debd8d077200755a1c
Carlos Rojas <76499791+crojasp10@users.noreply.github.com>
2025-10-07 10:50:05 -0500

    Add files via upload
    addying the project to Develop

---
+++ src/main/java/com/authService/authService/application/service/UserServiceImpl.java
@@ -0,0 +1,63 @@
+package com.authService.authService.application.service;
+
+import com.authService.authService.domain.port.RoleRepository;
+import com.authService.authService.domain.port.UserRepository;
+import com.authService.authService.domain.service.UserService;
+import com.authService.authService.infraestructure.out.RoleEntity;
+import com.authService.authService.infraestructure.out.UserEntity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<UserEntity> findAll() {
+        return (List<UserEntity>) userRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public UserEntity save(UserEntity user) {
+
+        Optional<RoleEntity> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+        List<RoleEntity> roles = new ArrayList();
+        optionalRoleUser.ifPresent(roles::add);
+
+
+        if(user.isAdmin()){
+            Optional<RoleEntity> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(roles::add);
+        }
+
+        user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+   /*  @Override
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    } */
+
+
+
+
+}
