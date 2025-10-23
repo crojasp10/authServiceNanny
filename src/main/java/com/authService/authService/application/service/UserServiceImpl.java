c58412473d1e8615574148a85637d1b09ab9ae91
Carlos Rojas <76499791+crojasp10@users.noreply.github.com>
2025-10-23 15:04:06 -0500

    Update UserServiceImpl.java

--- src/main/java/com/authService/authService/application/service/UserServiceImpl.java
+++ src/main/java/com/authService/authService/application/service/UserServiceImpl.java
@@ -51,13 +51,4 @@ public class UserServiceImpl implements UserService {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
-
-   /*  @Override
-    public boolean existsByUsername(String username) {
-        return userRepository.existsByUsername(username);
-    } */
-
-
-
-
 }
